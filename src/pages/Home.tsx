24e8cddf211c5896a9dcde144d89102fe536787b
Ricardo Santos <rick.giometti@hotmail.com>
2026-08-21 15:10:07 -0300

    Add ceremony date/venue line to home page, shrink logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import logoLR from '../assets/images/logo-LR.svg'
 import flowerBackground from '../assets/images/flower-background.webp'
 import './Home.css'
 
-const WEDDING_DATE = new Date('2026-10-31T16:00:00-03:00')
+const WEDDING_DATE = new Date('2026-10-31T15:30:00-03:00')
 
 function Home() {
   return (
@@ -13,7 +13,10 @@ function Home() {
         <div className="home__hero-content">
           <img className="home__logo" src={logoLR} alt="Laiana & Ricardo" />
           <h1 className="home__names">Laiana &amp; Ricardo</h1>
-          <p className="home__date">31 de Outubro de 2026</p>
+          <p className="home__date">31 de outubro de 2026 · 15h30</p>
+          <p className="home__local">
+            Espaço Araucária Village · Piracaia, SP
+          </p>
           <Countdown targetDate={WEDDING_DATE} />
         </div>
         <FloresMobile />
